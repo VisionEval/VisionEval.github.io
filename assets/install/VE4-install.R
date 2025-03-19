d635d1de0f355275d0960f66134b8fe9d183c305
Jeremy Raw <4449013+jrawbits@users.noreply.github.com>
2025-03-19 09:53:38 -0400

    Fixed path to copy code javascript

--- assets/install/VE4-install.R
+++ assets/install/VE4-install.R
@@ -1,1 +1,5 @@
+# Placeholder for VisionEval-4.0 installation script.
+# This script will soon be replaced by the real thing. It needs to be
+# synchronized with the development version located in the VisionEval
+# repository at sources\framework\VEStart\inst\download\VE4-install.R
 message("You successfully ran the VE-install.R script from the website")
--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -1,9 +1,11 @@
 // This assumes that you're using Rouge; if not, update the selector
-const codeBlocks = document.querySelectorAll('.language-R + .highlighter-rouge');
+// It also assumes there is a one-to-one relationship between
+// codeBlocks and copyCodeButtons on the current page.
+const codeBlocks = document.querySelectorAll('.language-R.highlighter-rouge');
 const copyCodeButtons = document.querySelectorAll('.copy-code-button');
 
 copyCodeButtons.forEach((copyCodeButton, index) => {
-        const code = codeBlocks[index].innerText;
+        const code = codeBlocks[index].innerText; // just grabs the first one...
 
         copyCodeButton.addEventListener('click', () => {
     // Copy the code to the user's clipboard
